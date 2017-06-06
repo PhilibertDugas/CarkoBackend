f9f1ebdee351193015d88a74e8b6a8dc0de42fac
Philibert Dugas <philibert.dugas@gmail.com>
2017-06-05 21:35:16 -0400

    Add active reservations controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   resources :parkings
   resources :customers, except: [:index] do
     resources :reservations, controller: :customer_reservations
+    get :active_reservations, controller: :customer_active_reservations, only: [:index]
+
     resources :parkings, controller: :customer_parkings
     resources :sources, only: [:create]
     resources :accounts, only: [:create]
     resources :vehicules, except: [:index]
+
+
     post :default_source, controller: :sources
     post :external, controller: :accounts
   end
