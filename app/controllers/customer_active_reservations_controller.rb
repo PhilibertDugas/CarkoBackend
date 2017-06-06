ccc95ffe558db66a9739f2f72c57fab628bac7dc
Philibert Dugas <philibert.dugas@gmail.com>
2017-06-05 22:30:17 -0400

    Small fix

--- app/controllers/customer_active_reservations_controller.rb
+++ app/controllers/customer_active_reservations_controller.rb
@@ -1,6 +1,6 @@
 class CustomerActiveReservationsController < CustomerAreaController
   def index
-    @reservations = Reservation.where(customer_id: @customer.id, is_active: true)
+    @reservations = Reservation.where(customer_id: @customer.id).where(is_active: true)
     render json: @reservations
   end
 end
