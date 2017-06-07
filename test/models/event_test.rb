ad175c6d4f38693c7ad218853c23a6c97af493b6
Philibert Dugas <philibert.dugas@gmail.com>
2017-06-06 22:33:24 -0400

    Add more events

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   test "ordered_events returns the events ordered by start_time" do
     events = Event.ordered_events
-    assert_equal events(:canadien), events.first
-    assert_equal events(:alouette), events.last
+    assert events.first.start_time < events.last.start_time
   end
 end
