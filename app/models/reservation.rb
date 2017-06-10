2ee51b33ec16cc888f671af2eea6edccf8875398
Philibert Dugas <philibert.dugas@gmail.com>
2017-06-10 00:31:10 -0400

    inline for now notif

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -26,6 +26,14 @@ class Reservation < ApplicationRecord
       save
     end
     FreeParkingJob.set(wait_until: stop_time).perform_later(self)
-    NotifyParkingRecipientNewReservationJob.perform_later(self)
+    if token = parking.customer.token
+      NotificationPushService.send(token, reservation_success_message)
+    end
+  end
+
+  private
+
+  def reservation_success_message
+    "Congratulations, you have a new reservation on #{start_time}"
   end
 end
