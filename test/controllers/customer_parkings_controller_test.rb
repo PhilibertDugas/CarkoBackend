455bbed0779a6745820b5bc07118c507db00a0a2
Philibert Dugas <philibert.dugas@gmail.com>
2017-06-13 08:40:47 -0400

    fix multiple urls

--- test/controllers/customer_parkings_controller_test.rb
+++ test/controllers/customer_parkings_controller_test.rb
@@ -23,6 +23,26 @@ class CustomerParkingsControllerTest < ActionDispatch::IntegrationTest
     assert_response 201
   end
 
+  test "#create should accept an array of photo urls" do
+    assert_difference('Parking.count') do
+      post customer_parkings_url(@customer), params: {
+        parking: {
+          address: 'Super Unique Address',
+          customer_id: @parking.customer_id,
+          latitude: @parking.latitude,
+          longitude: @parking.longitude,
+          photo_url: @parking.photo_url,
+          price: @parking.price,
+          multiple_photo_urls: ['hey', 'hueh', 'hueeh']
+        }
+      }, headers: auth_headers, as: :json
+    end
+
+    assert_response 201
+    inserted_parking = Parking.find_by(address: 'Super Unique Address')
+    assert_equal 3, inserted_parking.multiple_photo_urls.count
+  end
+
   test "should update parking" do
     patch customer_parking_url(@customer, @parking), params: {
       parking: {
